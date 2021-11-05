8c55c9ac0f31259eb330370dd9bcab114c73c497
Ilya758 <subscriber6436@gmail.com>
2021-11-05 20:44:03 +0300

    feat: added force folder remove in 04-copy-directory index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,23 +4,30 @@ const fsPromises = fs.promises;
 const copyFile = fsPromises.copyFile;
 const path = require('path');
 
-(function copyDir() {
-    fs.mkdir(path.join(__dirname, 'files-copy'), {
-        recursive: true,
-    }, err => {
-        if (err) {
-            throw new Error('Folder already exists or it maybe another problem with it');
+(async function copyDir() {
+    await fsPromises.rm(
+        path.join(__dirname, 'files-copy'),
+        { recursive: true, force: true },
+        err => {
+            if (err) throw err;
         }
-        console.log('Folder successfully created.');
-    });
-
-    fsPromises
-        .readdir(path.join(__dirname, 'files'))
-        .then(files => {
-            files.forEach(file => {
-                const filePath = path.join(__dirname, 'files', file);
-                copyFile(filePath, path.join(__dirname, 'files-copy', file));
-                console.log(file);
-            });
+    );
+    await fsPromises.mkdir(
+        path.join(__dirname, 'files-copy'),
+        { recursive: true },
+        err => {
+            if (err) {
+                throw new Error(
+                    'Folder already exists or it maybe another problem with it'
+                );
+            }
+            console.log('Folder successfully created.');
+        }
+    );
+    await fsPromises.readdir(path.join(__dirname, 'files')).then(files => {
+        files.forEach(file => {
+            const filePath = path.join(__dirname, 'files', file);
+            copyFile(filePath, path.join(__dirname, 'files-copy', file));
         });
+    });
 })();
